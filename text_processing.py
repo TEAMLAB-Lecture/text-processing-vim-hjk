fd3a85d8911213f98fc6748c4783dec22e9145a7
HJ_Kim <dr_moms@naver.com>
2021-01-19 10:44:15 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -58,5 +58,5 @@ def no_vowels(input_string):
             >>> tp.normalize(input_string2)
             ''W lv Pythn!'
     """
-    no_vowel_string = input_string.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+    no_vowel_string = input_string.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '').replace('A', '').replace('E', '').replace('I', '').replace('O', '').replace('U', '')
     return no_vowel_string
